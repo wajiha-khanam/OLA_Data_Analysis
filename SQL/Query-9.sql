7bbb3ed5f5a3634cf36c3d54ffbfd09e151a918d
wajiha-khanam <wajihakhanam2001@gmail.com>
2024-11-22 20:44:54 +0530

    Update Query-9.sql

--- SQL/Query-9.sql
+++ SQL/Query-9.sql
@@ -1,3 +1,5 @@
+-- 9. Calculate the total booking value of rides completed successfully:
+
 select sum(booking_value) as total_successful_ride_value
 from ride_booking_data
 where booking_status = 'Success'
