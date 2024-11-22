a7537752c2767985330563ed537c72b7b027a859
wajiha-khanam <wajihakhanam2001@gmail.com>
2024-11-22 20:43:08 +0530

    Update Query-5.sql

--- SQL/Query-5.sql
+++ SQL/Query-5.sql
@@ -1,1 +1,3 @@
+-- 5. Get the number of rides cancelled by drivers due to personal and car-related issues:
+
 select count(*) from ride_booking_data where canceled_rides_by_driver = 'Personal & Car related issue'
