4532a798308a133e0f9d1447c1f34029fa52ea79
wajiha-khanam <wajihakhanam2001@gmail.com>
2024-11-22 20:43:37 +0530

    Update Query-6.sql

--- SQL/Query-6.sql
+++ SQL/Query-6.sql
@@ -1,3 +1,5 @@
+-- 6. Find the maximum and minimum driver ratings for Prime Sedan bookings:
+
 select max(Driver_Ratings) as max_rating, min(Driver_Ratings) as min_rating
 from ride_booking_data
 where vehicle_type = 'Prime Sedan'
